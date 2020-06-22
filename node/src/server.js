323fd1e6e01be53ac4093ac171d7008d82b94580
yuta0709 <yf20030709@gmail.com>
2020-06-22 23:29:01 +0900

    Initial commit

---
+++ node/src/server.js
@@ -0,0 +1,9 @@
+'use strict';
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write(`Hello ${req.url}`);
+    res.end();
+})
+
+server.listen(8080);
